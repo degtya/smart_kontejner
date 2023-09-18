feb5206a403545264b70d5d18dc10bb055e71ad6
degtya <105122837+degtya@users.noreply.github.com>
2023-09-18 10:14:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const optionButtons = document.querySelectorAll(".option-button");
-    
-    optionButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            optionButtons.forEach(function(btn) {
-                btn.classList.remove("selected");
+document.addEventListener('DOMContentLoaded', function () {
+    const questionContainers = document.querySelectorAll('.input-container');
+
+    questionContainers.forEach(container => {
+        const question = container.querySelector('.question label').textContent.trim();
+        const response = container.querySelector('.response');
+        const hiddenInput = container.querySelector('input[type="hidden"]');
+        const optionButtons = container.querySelectorAll('.option-button');
+
+        optionButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const selectedValue = button.getAttribute('data-value');
+                hiddenInput.value = selectedValue;
+
+                // Подсветите выбранный вариант ответа
+                optionButtons.forEach(btn => {
+                    btn.classList.remove('selected');
+                });
+                button.classList.add('selected');
+
+                // Выведите выбранный вопрос и ответ в консоль (для проверки)
+                console.log('Вопрос:', question);
+                console.log('Ответ:', selectedValue);
             });
-            
-            this.classList.add("selected");
-            
-            const selectedValue = this.getAttribute("data-value");
-            // Теперь можно использовать selectedValue для сохранения выбранного значения
-            console.log("Выбрано: " + selectedValue);
         });
     });
 });
