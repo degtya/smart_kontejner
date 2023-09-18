8bdfff9da0f3b5efe7b3a75f2f98c6a621acfd46
degtya <105122837+degtya@users.noreply.github.com>
2023-09-18 09:00:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-// Найти модальное окно и кнопку для закрытия
-var modal = document.getElementById('myModal');
-var closeBtn = document.getElementsByClassName('close')[0];
-
-// Когда кликают на кнопку Poptat cenu, отобразить модальное окно
-var popButton = document.querySelector('input[type="submit"]');
-popButton.onclick = function() {
-    modal.style.display = 'block';
-}
-
-// Когда кликают на крестик, скрыть модальное окно
-closeBtn.onclick = function() {
-    modal.style.display = 'none';
-}
-
-// Когда кликают вне модального окна, скрыть его
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-}
-
-// Обработчик отправки формы
-var form = document.getElementById('myForm');
-form.onsubmit = function(event) {
-    event.preventDefault(); // Предотвратить отправку формы по умолчанию
-    // Здесь вы можете добавить код для отправки данных на сервер
-    // Например, отправить данные асинхронным запросом с использованием fetch() или XMLHttpRequest
-    alert('Данные отправлены!'); // Здесь вы можете заменить на реальную логику отправки данных
-    modal.style.display = 'none'; // Закрыть модальное окно после отправки данных
-}
-
+document.addEventListener("DOMContentLoaded", function () {
+    const optionButtons = document.querySelectorAll(".option-button");
+    
+    optionButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            optionButtons.forEach(function(btn) {
+                btn.classList.remove("selected");
+            });
+            
+            this.classList.add("selected");
+            
+            const selectedValue = this.getAttribute("data-value");
+            // Теперь можно использовать selectedValue для сохранения выбранного значения
+            console.log("Выбрано: " + selectedValue);
+        });
+    });
+});
