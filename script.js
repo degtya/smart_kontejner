5311e01385974a329d924c01a6e13e10e5108520
degtya <105122837+degtya@users.noreply.github.com>
2023-09-19 10:16:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -34,34 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    totalWaterVolumeInput.addEventListener('input', function () {
-        const placeholderText = 'Celková kapacita bude spočítána podle vybraného objemu';
-
-        if (this.value !== '') {
-            totalCapacityInput.style.visibility = 'hidden'; // Скрываем элементы ввода
-            totalCapacityInput.nextElementSibling.style.visibility = 'hidden'; // Скрываем единицы измерения
-            totalCapacityInput.value = ''; // Очищаем его значение
-            totalCapacityInput.setAttribute('placeholder', placeholderText); // Устанавливаем текст placeholder
-            console.log('placehoder', placeholderText);
-
-            // Скрываем вопрос и описание
-            const questionContainer = totalCapacityInput.closest('.input-container');
-            const question = questionContainer.querySelector('.question');
-            question.style.display = 'none';
-            questionContainer.querySelector('.description').style.display = 'none';
-        } else {
-            totalCapacityInput.style.visibility = 'visible'; // Показываем элементы ввода
-            totalCapacityInput.nextElementSibling.style.visibility = 'visible'; // Показываем единицы измерения
-            totalCapacityInput.removeAttribute('placeholder'); // Удаляем текст placeholder
-
-            // Восстанавливаем вопрос и описание
-            const questionContainer = totalCapacityInput.closest('.input-container');
-            const question = questionContainer.querySelector('.question');
-            question.style.display = 'block';
-            questionContainer.querySelector('.description').style.display = 'block';
-        }
-    });
-    
+ 
     const questionContainers = document.querySelectorAll('.input-container');
 
     questionContainers.forEach(container => {
@@ -87,20 +60,10 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
-  
-    
-    const nicCheckbox = document.getElementById("nic");
-    const otherCheckboxes = document.querySelectorAll('[name="dispecink[]"]:not(#nic)');
 
-    // Добавляем обработчик события для чекбокса "Nic"
-    nicCheckbox.addEventListener("change", function () {
-        // Если "Nic" отмечен, снимаем галочки с других вариантов ответа
-        if (nicCheckbox.checked) {
-            otherCheckboxes.forEach((checkbox) => {
-                checkbox.checked = false;
-            });
-        }
-    });
+
 
 
 });
+
+  
